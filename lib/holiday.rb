8ea768dc2b0736da59f78fbfcc32f1f63ea1e89c
Jordan Reed <jordanreed03@gmail.com>
2020-12-21 18:16:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,30 @@ require 'pry'
   # }
   # return the second element in the 4th of July array
 
-def second_supply_for_fourth_of_july(holiday_supplies)
-holiday_supplies[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(hash)
+  hash[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-holiday_hash[:winter][:christmas] << supply
-holiday_hash[:winter][:new_years] << supply
+def add_supply_to_winter_holidays(hash, supply)
+  hash[:winter][:christmas] << supply
+  hash[:winter][:new_years] << supply
 end
 
 
-def add_supply_to_memorial_day(holiday_hash, supply)
-holiday_hash[:spring][:memorial_day] << supply
+def add_supply_to_memorial_day(hash, supply)
+  hash[:spring][:memorial_day] << supply
 end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-holiday_hash[season][holiday_name] = supply_array
+def add_new_holiday_with_supplies(hash, season, holiday, supply_array)
+  hash[season][holiday] = supply_array
 end
 
-def all_winter_holiday_supplies(holiday_hash)
-holiday_hash[:winter].values.flatten
+def all_winter_holiday_supplies(hash)
+  hash[:winter].values.flatten
 end 
 
-def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do | season, holiday|
+def all_supplies_in_holidays(hash)
+  hash.each do |season, holiday|
      puts "#{season.capitalize}:"
      holiday.each do |holiday, item|
        puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
@@ -49,8 +49,8 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-def all_holidays_with_bbq(holiday_hash)
- holiday_hash.map do |season, holiday|
+def all_holidays_with_bbq(hash)
+  hash.map do |season, holiday|
     holiday.map do |holiday, supply|
       holiday if supply.include?("BBQ")
     end 
